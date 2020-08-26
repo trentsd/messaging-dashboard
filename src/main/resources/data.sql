7ff26182db13191152421f95f9bad13ecac1f160
Sean Trent <sean.d.trent@gmail.com>
2020-08-26 13:29:09 -0400

    Inserted an admin user into the user table

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,11 @@
 insert into users (username, email, password, enabled)
-  values ('user', 'email@email.com', 'pass', true);
+  values ('user', 'user.email@email.com', 'pass', true);
 
 insert into authorities (username, authority)
   values ('user', 'ROLE_USER');
+
+insert into users (username, email, password, enabled)
+  values ('admin', 'admin.email@email.com', 'adminpass', true);
+
+insert into authorities (username, authority)
+  values ('admin', 'ROLE_ADMIN');
