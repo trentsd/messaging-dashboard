78637a8abd757a35fe9a361f1ab454b6102d6068
Sean Trent <sean.d.trent@gmail.com>
2020-09-07 15:02:16 -0400

    Added generated component for 'My Account' page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,14 @@ import { LoginComponent } from './login/login.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MyaccountComponent } from './myaccount/myaccount.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    MyaccountComponent
   ],
   imports: [
     BrowserModule,
