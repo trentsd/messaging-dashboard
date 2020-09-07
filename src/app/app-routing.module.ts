da30f669b54696d8dbbc1642ccf43b1fe2f3ba58
Sean Trent <sean.d.trent@gmail.com>
2020-09-07 15:11:25 -0400

    Skeleton 'My Account' page added to routing module and main nav bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { MyaccountComponent } from './myaccount/myaccount.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home'},
   { path: 'home', component: HomeComponent},
-  { path: 'login', component: LoginComponent}
+  { path: 'login', component: LoginComponent},
+  { path: 'myaccount', component: MyaccountComponent}
 ];
 
 @NgModule({
