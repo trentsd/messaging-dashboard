90bf7e5f7054ed7138fae753e9ab3c5937ae93a7
Sean Trent <sean.d.trent@gmail.com>
2020-10-12 11:30:19 -0400

    User table work in progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MyaccountComponent } from './myaccount/myaccount.component';
 import { RegisterComponent } from './register/register.component';
+import { UserTableComponent } from './home/user-table/user-table.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { RegisterComponent } from './register/register.component';
     HomeComponent,
     LoginComponent,
     MyaccountComponent,
-    RegisterComponent
+    RegisterComponent,
+    UserTableComponent
   ],
   imports: [
     BrowserModule,
