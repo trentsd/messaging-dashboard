7be36cda237f7abed82a6025d31bd8232951876a
Sean Trent <sean.d.trent@gmail.com>
2020-10-16 15:16:17 -0400

    The home component now GETs from home/api to retrieve a list of usernames to display in the user-table component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,6 +4,8 @@ import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { trimTrailingNulls } from '@angular/compiler/src/render3/view/util';
 
+import { User } from '../shared/interfaces/user.model';
+
 //This interface is just to appease angular while I play with different functionalities.
 export interface Greeting {
   id: number;
@@ -20,13 +22,17 @@ export class HomeComponent implements OnInit {
   title = 'Demo';
   greeting: Greeting = {id: null, content: null};
 
+  userList: any;
+
   constructor(private app: AppService, private http: HttpClient) {}
 
   authenticated() { return this.app.authenticated; }
 
   ngOnInit(): void {
+    this.http.get('home/api').subscribe(data => {this.userList = data;});
+
     if (this.authenticated()) {
-    this.http.get('home').subscribe((data: Greeting) =>
+    this.http.get('home/api').subscribe((data: Greeting) =>
       this.greeting = data
       );
     }
--- src/app/shared/interfaces/user.model.ts
+++ src/app/shared/interfaces/user.model.ts
@@ -1,13 +1,7 @@
 /**
  * Temporary interface for user.
- * Will at some point have an image
  * TODO: 1. add image
  */
 export interface User {
-  id: number;
-  userName: string;
-  userFirstName: string;
-  userLastName: string;
-  isEmailAvailable: boolean;
-  isTextAvailable: boolean;
+  USERNAME: string;
 }
