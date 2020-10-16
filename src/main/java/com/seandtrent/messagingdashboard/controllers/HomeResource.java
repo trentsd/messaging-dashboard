a929787f1c96cb487e683d4a03915628989b832c
Sean Trent <sean.d.trent@gmail.com>
2020-10-16 14:18:33 -0400

    Changed the home controller to use jdbcTemplate to query for usernames

--- src/main/java/com/seandtrent/messagingdashboard/controllers/HomeResource.java
+++ src/main/java/com/seandtrent/messagingdashboard/controllers/HomeResource.java
@@ -1,9 +1,12 @@
 package com.seandtrent.messagingdashboard.controllers;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,17 +17,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeResource {
 
+
+  @Autowired
+  JdbcTemplate jdbcTemplate;
+
   /**
    * Functionaility for the API at "/" available to all visitors to the page.
    * TODO: Replace placeholder functionality
    * @return just placeholder html for now
    */
   @GetMapping("/home")
-  public Map<String, Object> home() {
-    Map<String, Object> model = new HashMap<String, Object>();
-    model.put("id", UUID.randomUUID().toString());
-    model.put("content", "Hello World");
-    return model;
+  public List<Map<String, Object>> home() {
+    return jdbcTemplate.queryForList("select username "
+                                + "from users");
+
+    // Map<String, Object> model = new HashMap<String, Object>();
+    // model.put("id", UUID.randomUUID().toString());
+    // model.put("content", "Hello World");
+    // return model;
 }
 
 }
--- src/main/java/com/seandtrent/messagingdashboard/models/User.java
+++ src/main/java/com/seandtrent/messagingdashboard/models/User.java
@@ -1,14 +1,7 @@
 package com.seandtrent.messagingdashboard.models;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-// @Entity
-// @Table(name = "users")
 public class User {
 
-  //@Id
   private String username;
 
   private String email;
